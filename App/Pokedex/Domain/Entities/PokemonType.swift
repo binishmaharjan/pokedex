e7e16fd0de6ada8ef13c4d357fee70a90915d10e
Maharjan Binish <legendbinish124@gmail.om>
2021-07-06 01:17:17 +0900

    Fix All Weakness Value

--- App/Pokedex/Domain/Entities/PokemonType.swift
+++ App/Pokedex/Domain/Entities/PokemonType.swift
@@ -31,30 +31,30 @@ enum Type: String, Codable, Equatable {
         switch self {
         case .normal:
             return [
-                .normal: 1.0,   .fighting: 1.0,  .flying: 1.0,
-                .poison: 0.5,   .ground: 2.0,    .rock: 1.0,
-                .bug: 0.5,      .ghost: 0.0,     .steel: 1.0,
-                .fire: 1.0,     .water: 1.0,     .grass: 0.5,
-                .electric: 1.0, .psychic: 2.0,   .ice: 1.0,
-                .dragon: 1.0,   .dark: 1.0,      .fairy: 0.5
+                .normal: 1.0,   .fighting: 2.0,  .flying: 1.0,
+                .poison: 1.0,   .ground: 1.0,    .rock: 1.0,
+                .bug: 1.0,      .ghost: 0.0,     .steel: 1.0,
+                .fire: 1.0,     .water: 1.0,     .grass: 1.0,
+                .electric: 1.0, .psychic: 1.0,   .ice: 1.0,
+                .dragon: 1.0,   .dark: 1.0,      .fairy: 1.0
             ]
         case .fighting:
             return [
-                .normal: 1.0,   .fighting: 0.5,  .flying: 2.0,
-                .poison: 0.5,   .ground: 2.0,    .rock: 0.5,
-                .bug: 0.25,     .ghost: 1.0,     .steel: 1.0,
-                .fire: 1.0,     .water: 1.0,     .grass: 0.5,
-                .electric: 1.0, .psychic: 4.0,   .ice: 1.0,
-                .dragon: 1.0,   .dark: 0.5,      .fairy: 1.0
+                .normal: 1.0,   .fighting: 1.0,  .flying: 2.0,
+                .poison: 1.0,   .ground: 1.0,    .rock: 0.5,
+                .bug: 0.5,     .ghost: 1.0,     .steel: 1.0,
+                .fire: 1.0,     .water: 1.0,     .grass: 1.0,
+                .electric: 1.0, .psychic: 2.0,   .ice: 1.0,
+                .dragon: 1.0,   .dark: 0.5,      .fairy: 2.0
             ]
         case .flying:
             return [
-                .normal: 1.0,   .fighting: 0.25, .flying: 1.0,
-                .poison: 0.5,   .ground: 0.0,    .rock: 2.0,
-                .bug: 0.25,     .ghost: 1.0,     .steel: 1.0,
-                .fire: 1.0,     .water: 1.0,     .grass: 0.25,
-                .electric: 2.0, .psychic: 2.0,   .ice: 2.0,
-                .dragon: 1.0,   .dark: 1.0,      .fairy: 0.5
+                .normal: 1.0,   .fighting: 0.5, .flying: 1.0,
+                .poison: 1.0,   .ground: 0.0,    .rock: 2.0,
+                .bug: 0.5,     .ghost: 1.0,     .steel: 1.0,
+                .fire: 1.0,     .water: 1.0,     .grass: 0.5,
+                .electric: 2.0, .psychic: 1.0,   .ice: 2.0,
+                .dragon: 1.0,   .dark: 1.0,      .fairy: 1.0
             ]
         case .poison:
             return [
@@ -67,129 +67,129 @@ enum Type: String, Codable, Equatable {
             ]
         case .ground:
             return [
-                .normal: 1.0,   .fighting: 0.5,  .flying: 1.0,
-                .poison: 0.25,  .ground: 2.0,    .rock: 0.5,
-                .bug: 0.5,      .ghost: 1.0,     .steel: 1.0,
-                .fire: 1.0,     .water: 2.0,     .grass: 1.0,
-                .electric: 0.0, .psychic: 2.0,   .ice: 2.0,
-                .dragon: 1.0,   .dark: 1.0,      .fairy: 0.5
+                .normal: 1.0,   .fighting: 1.0,  .flying: 1.0,
+                .poison: 0.5,   .ground: 1.0,    .rock: 0.5,
+                .bug: 1.0,      .ghost: 1.0,     .steel: 1.0,
+                .fire: 1.0,     .water: 2.0,     .grass: 2.0,
+                .electric: 0.0, .psychic: 1.0,   .ice: 2.0,
+                .dragon: 1.0,   .dark: 1.0,      .fairy: 1.0
             ]
         case .rock:
             return [
-                .normal: 0.5,   .fighting: 1.0,  .flying: 0.5,
-                .poison: 0.25,  .ground: 4.0,    .rock: 1.0,
-                .bug: 0.5,      .ghost: 1.0,     .steel: 2.0,
-                .fire: 0.5,     .water: 2.0,     .grass: 1.0,
-                .electric: 1.0, .psychic: 2.0,   .ice: 1.0,
-                .dragon: 1.0,   .dark: 1.0,      .fairy: 0.5
+                .normal: 0.5,   .fighting: 2.0,  .flying: 0.5,
+                .poison: 0.5,   .ground: 2.0,    .rock: 1.0,
+                .bug: 1.0,      .ghost: 1.0,     .steel: 2.0,
+                .fire: 0.5,     .water: 2.0,     .grass: 2.0,
+                .electric: 1.0, .psychic: 1.0,   .ice: 1.0,
+                .dragon: 1.0,   .dark: 1.0,      .fairy: 1.0
             ]
         case .bug:
             return [
-                .normal: 1.0,   .fighting: 0.25, .flying: 2.0,
-                .poison: 0.5,   .ground: 1.0,    .rock: 2.0,
-                .bug: 0.5,      .ghost: 1.0,     .steel: 1.0,
-                .fire: 2.0,     .water: 1.0,     .grass: 0.25,
-                .electric: 1.0, .psychic: 2.0,   .ice: 1.0,
-                .dragon: 1.0,   .dark: 1.0,      .fairy: 0.5
+                .normal: 1.0,   .fighting: 0.5,  .flying: 2.0,
+                .poison: 1.0,   .ground: 0.5,    .rock: 2.0,
+                .bug: 1.0,      .ghost: 1.0,     .steel: 1.0,
+                .fire: 2.0,     .water: 1.0,     .grass: 0.5,
+                .electric: 1.0, .psychic: 1.0,   .ice: 1.0,
+                .dragon: 1.0,   .dark: 1.0,      .fairy: 1.0
             ]
         case .ghost:
             return [
                 .normal: 0.0,   .fighting: 0.0,  .flying: 1.0,
-                .poison: 0.25,  .ground: 2.0,    .rock: 1.0,
-                .bug: 0.25,     .ghost: 2.0,     .steel: 1.0,
-                .fire: 1.0,     .water: 1.0,     .grass: 0.5,
-                .electric: 1.0, .psychic: 2.0,   .ice: 1.0,
-                .dragon: 1.0,   .dark: 2.0,      .fairy: 0.5
+                .poison: 0.5,   .ground: 1.0,    .rock: 1.0,
+                .bug: 0.5,      .ghost: 2.0,     .steel: 1.0,
+                .fire: 1.0,     .water: 1.0,     .grass: 1.0,
+                .electric: 1.0, .psychic: 1.0,   .ice: 1.0,
+                .dragon: 1.0,   .dark: 2.0,      .fairy: 1.0
             ]
         case .steel:
             return [
-                .normal: 0.5,   .fighting: 1.0,  .flying: 0.5,
-                .poison: 0.0,   .ground: 4.0,    .rock: 0.5,
-                .bug: 0.25,     .ghost: 1.0,     .steel: 0.5,
-                .fire: 2.0,     .water: 1.0,     .grass: 0.25,
-                .electric: 1.0, .psychic: 1.0,   .ice: 0.5,
-                .dragon: 0.5,   .dark: 1.0,      .fairy: 0.25
+                .normal: 0.5,   .fighting: 2.0,  .flying: 0.5,
+                .poison: 0.0,   .ground: 2.0,    .rock: 0.5,
+                .bug: 0.5,      .ghost: 1.0,     .steel: 0.5,
+                .fire: 2.0,     .water: 1.0,     .grass: 0.5,
+                .electric: 1.0, .psychic: 0.5,   .ice: 0.5,
+                .dragon: 0.5,   .dark: 1.0,      .fairy: 0.5
             ]
         case .fire:
             return [
-                .normal: 1.0,   .fighting: 0.5,  .flying: 1.0,
-                .poison: 0.5,   .ground: 4.0,    .rock: 2.0,
-                .bug: 0.25,     .ghost: 1.0,     .steel: 0.5,
-                .fire: 0.5,     .water: 2.0,     .grass: 0.25,
-                .electric: 1.0, .psychic: 2.0,   .ice: 0.5,
-                .dragon: 1.0,   .dark: 1.0,      .fairy: 0.25
+                .normal: 1.0,   .fighting: 1.0,  .flying: 1.0,
+                .poison: 1.0,   .ground: 2.0,    .rock: 2.0,
+                .bug: 0.5,      .ghost: 1.0,     .steel: 0.5,
+                .fire: 0.5,     .water: 2.0,     .grass: 0.5,
+                .electric: 1.0, .psychic: 1.0,   .ice: 0.5,
+                .dragon: 1.0,   .dark: 1.0,      .fairy: 0.5
             ]
         case .water:
             return [
-                .normal: 1.0,   .fighting: 0.5,  .flying: 1.0,
-                .poison: 0.5,   .ground: 2.0,    .rock: 1.0,
-                .bug: 0.5,      .ghost: 1.0,     .steel: 0.5,
-                .fire: 0.5,     .water: 0.5,     .grass: 1.0,
-                .electric: 2.0, .psychic: 2.0,   .ice: 0.5,
-                .dragon: 1.0,   .dark: 1.0,      .fairy: 0.5
+                .normal: 1.0,   .fighting: 1.0,  .flying: 1.0,
+                .poison: 1.0,   .ground: 1.0,    .rock: 1.0,
+                .bug: 1.0,      .ghost: 1.0,     .steel: 0.5,
+                .fire: 0.5,     .water: 0.5,     .grass: 2.0,
+                .electric: 2.0, .psychic: 1.0,   .ice: 0.5,
+                .dragon: 1.0,   .dark: 1.0,      .fairy: 1.0
             ]
         case .grass:
             return [
-                .normal: 1.0,   .fighting: 0.5,  .flying: 2.0,
-                .poison: 1.0,   .ground: 1.0,    .rock: 1.0,
-                .bug: 1.0,      .ghost: 1.0,     .steel: 1.0,
-                .fire: 2.0,     .water: 0.5,     .grass: 0.25,
-                .electric: 0.5, .psychic: 2.0,   .ice: 2.0,
-                .dragon: 1.0,   .dark: 1.0,      .fairy: 0.5
+                .normal: 1.0,   .fighting: 1.0,  .flying: 2.0,
+                .poison: 2.0,   .ground: 0.5,    .rock: 1.0,
+                .bug: 2.0,      .ghost: 1.0,     .steel: 1.0,
+                .fire: 2.0,     .water: 0.5,     .grass: 0.5,
+                .electric: 0.5, .psychic: 1.0,   .ice: 2.0,
+                .dragon: 1.0,   .dark: 1.0,      .fairy: 1.0
             ]
         case .electric:
             return [
-                .normal: 1.0,   .fighting: 0.5,  .flying: 0.5,
-                .poison: 0.5,   .ground: 4.0,    .rock: 1.0,
-                .bug: 0.5,      .ghost: 1.0,     .steel: 0.5,
-                .fire: 1.0,     .water: 1.0,     .grass: 0.5,
-                .electric: 0.5, .psychic: 2.0,   .ice: 1.0,
-                .dragon: 1.0,   .dark: 1.0,      .fairy: 0.5
+                .normal: 1.0,   .fighting: 1.0,  .flying: 0.5,
+                .poison: 1.0,   .ground: 2.0,    .rock: 1.0,
+                .bug: 1.0,      .ghost: 1.0,     .steel: 0.5,
+                .fire: 1.0,     .water: 1.0,     .grass: 1.0,
+                .electric: 0.5, .psychic: 1.0,   .ice: 1.0,
+                .dragon: 1.0,   .dark: 1.0,      .fairy: 1.0
             ]
         case .psychic:
             return [
-                .normal: 1.0,   .fighting: 0.25,  .flying: 1.0,
-                .poison: 0.5,   .ground: 2.0,    .rock: 1.0,
-                .bug: 1.0,      .ghost: 2.0,     .steel: 1.0,
-                .fire: 1.0,     .water: 1.0,     .grass: 0.5,
-                .electric: 1.0, .psychic: 1.0,   .ice: 1.0,
-                .dragon: 1.0,   .dark: 2.0,      .fairy: 0.5
+                .normal: 1.0,   .fighting: 0.5,  .flying: 1.0,
+                .poison: 1.0,   .ground: 1.0,    .rock: 1.0,
+                .bug: 2.0,      .ghost: 2.0,     .steel: 1.0,
+                .fire: 1.0,     .water: 1.0,     .grass: 1.0,
+                .electric: 1.0, .psychic: 0.5,   .ice: 1.0,
+                .dragon: 1.0,   .dark: 2.0,      .fairy: 1.0
             ]
         case .ice:
             return [
-                .normal: 1.0,   .fighting: 1.0,  .flying: 1.0,
-                .poison: 0.5,   .ground: 2.0,    .rock: 2.0,
-                .bug: 0.5,      .ghost: 1.0,     .steel: 2.0,
-                .fire: 2.0,     .water: 1.0,     .grass: 0.5,
-                .electric: 1.0, .psychic: 2.0,   .ice: 0.5,
-                .dragon: 1.0,   .dark: 1.0,      .fairy: 0.5
+                .normal: 1.0,   .fighting: 2.0,  .flying: 1.0,
+                .poison: 1.0,   .ground: 1.0,    .rock: 2.0,
+                .bug: 1.0,      .ghost: 1.0,     .steel: 2.0,
+                .fire: 2.0,     .water: 1.0,     .grass: 1.0,
+                .electric: 1.0, .psychic: 1.0,   .ice: 0.5,
+                .dragon: 1.0,   .dark: 1.0,      .fairy: 1.0
             ]
         case .dragon:
             return [
-                .normal: 1.0,   .fighting: 0.5,  .flying: 1.0,
-                .poison: 0.5,   .ground: 2.0,    .rock: 1.0,
-                .bug: 0.5,      .ghost: 1.0,     .steel: 1.0,
-                .fire: 0.5,     .water: 0.5,     .grass: 0.25,
-                .electric: 0.5, .psychic: 2.0,   .ice: 2.0,
-                .dragon: 2.0,   .dark: 1.0,      .fairy: 1.0
+                .normal: 1.0,   .fighting: 1.0,  .flying: 1.0,
+                .poison: 1.0,   .ground: 1.0,    .rock: 1.0,
+                .bug: 1.0,      .ghost: 1.0,     .steel: 1.0,
+                .fire: 0.5,     .water: 0.5,     .grass: 0.5,
+                .electric: 0.5, .psychic: 1.0,   .ice: 2.0,
+                .dragon: 2.0,   .dark: 1.0,      .fairy: 2.0
             ]
         case .dark:
             return [
-                .normal: 1.0,   .fighting: 1.0,  .flying: 1.0,
-                .poison: 0.5,   .ground: 2.0,    .rock: 1.0,
-                .bug: 1.0,      .ghost: 0.5,     .steel: 1.0,
-                .fire: 1.0,     .water: 1.0,     .grass: 0.5,
+                .normal: 1.0,   .fighting: 2.0,  .flying: 1.0,
+                .poison: 1.0,   .ground: 1.0,    .rock: 1.0,
+                .bug: 2.0,      .ghost: 0.5,     .steel: 1.0,
+                .fire: 1.0,     .water: 1.0,     .grass: 1.0,
                 .electric: 1.0, .psychic: 0.0,   .ice: 1.0,
-                .dragon: 1.0,   .dark: 0.5,      .fairy: 1.0
+                .dragon: 1.0,   .dark: 0.5,      .fairy: 2.0
             ]
         case .fairy:
             return [
-                .normal: 1.0,   .fighting: 0.25,  .flying: 1.0,
-                .poison: 1.0,   .ground: 2.0,    .rock: 1.0,
-                .bug: 0.25,      .ghost: 1.0,     .steel: 2.0,
-                .fire: 1.0,     .water: 1.0,     .grass: 0.5,
-                .electric: 1.0, .psychic: 2.0,   .ice: 1.0,
-                .dragon: 0.0,   .dark: 0.5,      .fairy: 0.5
+                .normal: 1.0,   .fighting: 0.5,  .flying: 1.0,
+                .poison: 2.0,   .ground: 1.0,    .rock: 1.0,
+                .bug: 0.5,      .ghost: 1.0,     .steel: 2.0,
+                .fire: 1.0,     .water: 1.0,     .grass: 1.0,
+                .electric: 1.0, .psychic: 1.0,   .ice: 1.0,
+                .dragon: 0.0,   .dark: 0.5,      .fairy: 1.0
             ]
         }
     }
